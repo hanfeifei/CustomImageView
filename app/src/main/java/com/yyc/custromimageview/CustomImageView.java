daf553bdc55b3089c9e324067b2a0222d74becfb
hanfeifei <hanfeifei@goumin.com>
2016-07-11 21:42:32 +0800

    add setimage

--- app/src/main/java/com/yyc/custromimageview/CustomImageView.java
+++ app/src/main/java/com/yyc/custromimageview/CustomImageView.java
@@ -6,7 +6,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class CustomImageView extends ImageView {
@@ -15,6 +18,9 @@ public class CustomImageView extends ImageView {
      * 控件中的图片
      */
     private Bitmap mImage;
+    Rect bitmapRect;
+    int mWidth = -1;
+    int mHeight = -1;
 
     public CustomImageView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -50,7 +56,13 @@ public class CustomImageView extends ImageView {
         a.recycle();
     }
 
-    public void setImage() {
+    public void setImage(Drawable drawable) {
+        if (drawable == null) {
+            mImage = null;
+        } else {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            mImage = bitmapDrawable.getBitmap();
+        }
         invalidate();
         requestLayout();
     }
@@ -58,7 +70,18 @@ public class CustomImageView extends ImageView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Rect bitmapRect = new Rect(getWidth() - mImage.getWidth(), getHeight() - mImage.getHeight(), getWidth(), getHeight());
+        if (mImage == null) {
+            return;
+        }
+
+        int  h = getHeight();
+        int w = getWidth();
+
+        if (mWidth!=w||mHeight!=h) {
+            mWidth = w;
+            mHeight = h;
+            bitmapRect = new Rect(w - mImage.getWidth(), h - mImage.getHeight(), w, h);
+        }
         canvas.drawBitmap(mImage, null, bitmapRect, null);
     }
 
--- app/src/main/java/com/yyc/custromimageview/MainActivity.java
+++ app/src/main/java/com/yyc/custromimageview/MainActivity.java
@@ -1,15 +1,78 @@
 package com.yyc.custromimageview;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by HFF on 16/7/11.
  */
 public class MainActivity extends Activity {
+    ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_activity);
+        lv = (ListView) findViewById(R.id.lv);
+        lv.setAdapter(new MyAdapter(this));
+    }
+
+    class MyAdapter extends BaseAdapter {
+        Context context;
+
+        public MyAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return 40;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = View.inflate(context, R.layout.main, null);
+                holder.iv = (CustomImageView) convertView.findViewById(R.id.iv);
+                holder.tv = (TextView) convertView.findViewById(R.id.tv);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            Log.e("lzw","holder.iv " + (holder.iv==null));
+            Log.e("lzw","drawable " + (context.getResources().getDrawable(R.drawable.v_small)==null));
+
+            if (position % 5 == 0) {
+                holder.iv.setImage(context.getResources().getDrawable(R.drawable.v_small));
+            } else {
+                holder.iv.setImage(null);
+            }
+            return convertView;
+        }
+
+        class ViewHolder {
+            CustomImageView iv;
+            TextView tv;
+        }
     }
 }
